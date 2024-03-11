4ca039326f4f3a5a00fe17cac97f98a1633c8815
Usov_Artem <artemka063@gmail.com>
2024-03-11 12:10:29 +0400

    feat: add filtering of duplicate objects by id

--- api/api.ts
+++ api/api.ts
@@ -1,26 +1,55 @@
-import md5 from 'md5';
+import md5 from "md5";
 
-export const fetchProducts = async (params: { action: string; params: { offset: number; limit: number; }; }) => {
-    const timestamp = new Date().toISOString().split('T')[0].replace(/-/g, ''); 
+export type Product = {
+  brand: string;
+  id: string;
+  price: number;
+  product: string;
+};
+
+export const fetchProducts = async (params: {
+  action: string;
+  params: { offset: number; limit: number };
+}): Promise<Product[]> => {
+  const timestamp = new Date().toISOString().split("T")[0].replace(/-/g, "");
     const authString = md5(`Valantis_${timestamp}`); 
     const requestOptions = {
-      method: 'POST',
+    method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'X-Auth': authString,
+      "Content-Type": "application/json",
+      "X-Auth": authString,
       },
       body: JSON.stringify(params),
     };
   
     try {
-      const response = await fetch('http://api.valantis.store:40000/', requestOptions);
+    const response = await fetch(
+      "http://api.valantis.store:40000/",
+      requestOptions
+    );
       if (!response.ok) {
-        throw new Error('Ошибка при запросе к API');
+      throw new Error("Ошибка при запросе к API");
       }
       const data = await response.json();
       console.log(data.result);
       
-      return data.result;
+    const correctResult: { [key: string]: Product } = data.result.reduce(
+      (acc: { [key: string]: Product }, item: Product) => {
+        const id = typeof item === "string" ? item : item.id;
+        if (!acc[id]) {
+          acc[id] = item;
+        }
+        return acc;
+      },
+      {}
+    );
+
+    console.log(Object.values(correctResult));
+    const correctArr = Object.values(correctResult);
+
+    console.log(correctArr);
+
+    return correctArr as Product[];
     } catch (error) {
       console.error(error);
       return null;
